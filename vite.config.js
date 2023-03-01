fd945c446bb792cd9b459bcebfa6d8ee9f3356ab
Estiven Mazo <juan.mazo@imagemaker.com>
2023-03-01 15:24:19 -0500

    chore: config alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
+  publicDir: '../public',
+  build: '../dist',
   plugins: [react()],
+  server: {
+    port: 9731
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src/') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components/')
+      },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles/') },
+      { find: '@routes', replacement: path.resolve(__dirname, 'src/routes/') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks/') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages/') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/api/') },
+      {
+        find: '@helpers',
+        replacement: path.resolve(__dirname, 'src/helpers/')
+      },
+      {
+        find: '@context',
+        replacement: path.resolve(__dirname, 'src/context/')
+      },
+      {
+        find: '@images',
+        replacement: path.resolve(__dirname, 'public/assets/images/')
+      }
+    ]
+  }
 })
